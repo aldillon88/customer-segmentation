b6f23c40c6abaf5d3d9faca432d54840513b1a8b
aldillon09 <aldillon09@gmail.com>
2024-11-20 18:03:46 +0100

    Docker setup

--- app/page_1.py
+++ app/page_1.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import plotly.graph_objects as go
 
 import sys
 import os
@@ -13,8 +11,10 @@ if project_root not in sys.path:
 
 from st_functions import *
 
-df = load_data("../data/clean/clustered.csv") # for local development
-#df = load_data("/data/clean/clustered.csv")
+data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'clean', 'clustered.csv'))
+
+#df = load_data("../data/clean/clustered.csv") # for local development
+df = load_data(data_path) # In the Docker build this is /app/data/clean/clustered.csv
 
 # Create population averages for comparison metrics.
 pop_total = len(df)
@@ -24,10 +24,12 @@ pop_mean_spending_score = df['spending_score'].mean().astype('int')
 pop_mean_purchase_freq = df['purchase_frequency'].mean().astype('int')
 pop_mean_membership = df['membership_years'].mean().astype('int')
 
-clusters = df['cluster'].unique().tolist()
+clusters = df['cluster'].sort_values().unique().tolist()
 clusters.insert(0, 'All Segments')
+cluster_colors = [None, '#83C9FF', '#2568C9', '#F9ABAB']
 dimensions = df.select_dtypes('object').columns.sort_values().to_list()
 dimensions.pop(dimensions.index('spending_score_category'))
+dimensions.pop(dimensions.index('purchase_frequency_category'))
 dimensions = [dim.replace('_', ' ').title() for dim in dimensions]
 
 with st.sidebar:
@@ -37,6 +39,10 @@ with st.sidebar:
 
 if cluster != 'All Segments':
 	df = df[df['cluster'] == cluster]
+	col_index = clusters.index(cluster)
+	color = [cluster_colors[col_index]]
+else:
+	color = cluster_colors[1:]
 
 # Calculate metrics
 total = len(df)
@@ -46,7 +52,7 @@ mean_spending_score = df['spending_score'].mean().astype('int')
 mean_purchase_freq = df['purchase_frequency'].mean().astype('int')
 mean_membership = df['membership_years'].mean().astype('int')
 
-# Create a list of metrics
+# Create a list of metrics (tuples)
 metrics = [
 	('Total Customers', total, pop_total),
 	('Mean Age', mean_age, pop_mean_age),
@@ -56,8 +62,6 @@ metrics = [
 	('Mean Membership Tenure', mean_membership, pop_mean_membership)
 ]
 
-# Calculate difference from population
-
 r1_cols = st.columns(6)
 
 for col, (metric, val, pop_val) in zip(r1_cols, metrics):
@@ -66,51 +70,35 @@ for col, (metric, val, pop_val) in zip(r1_cols, metrics):
 			with st.container(border=True):
 				if metric == 'Total Customers':
 					diff = f"{int(val / pop_val * 100)}% of all customers"
-					st.metric(metric, val, diff, delta_color='off')
+					st.metric(metric, f"{val:,}", diff, delta_color='off')
 				else:
 					diff = f"{relative_percentage_difference(val, pop_val)}% vs. global mean"
-					st.metric(metric, val, diff)
+					st.metric(metric, f"{val:,}", diff)
 	else:
 		with col:
 			with st.container(border=True):
-				st.metric(metric, val)
+				st.metric(metric, f"{val:,}")
 
 
-#row1col1, row1col2, row1col3 = st.columns(3)
-
-#with row1col1:
-#	with st.container(border=True):
-#		bar = plot_bar(df, dimension)
-#		st.plotly_chart(bar)
-
-#with row1col2:
-#	with st.container(border=True):
-#		bubble = plot_bubble(df, dimension, 'spending_score')
-#		st.plotly_chart(bubble)
-
-#with row1col3:
-#	with st.container(border=True):
-#		sunburst = test(df, dimension, 'last_purchase_amount')
-#		st.plotly_chart(sunburst)
-
-c1, c2, c3, c4 = st.columns(4)
+c1, c2 = st.columns(2)
+c3, c4 = st.columns(2)
 
 with c1:
 	with st.container(border=True):
-		bar1 = plot_bar(df, dimension)
+		bar1 = plot_bar(df, dimension, color)
 		st.plotly_chart(bar1)
 
 with c2:
 	with st.container(border=True):
-		bubble1 = test(df, dimension, 'spending_score')
+		bubble1 = plot_bubble(df, dimension, 'spending_score', color)
 		st.plotly_chart(bubble1)
 
 with c3:
 	with st.container(border=True):
-		bubble2 = test(df, dimension, 'purchase_frequency')
+		bubble2 = plot_bubble(df, dimension, 'purchase_frequency', color)
 		st.plotly_chart(bubble2)
 
 with c4:
 	with st.container(border=True):
-		bubble3 = test(df, dimension, 'last_purchase_amount')
+		bubble3 = plot_bubble(df, dimension, 'last_purchase_amount', color)
 		st.plotly_chart(bubble3)
--- app/st_functions.py
+++ app/st_functions.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
@@ -12,48 +11,30 @@ def load_data(path):
 	return df
 
 
-def plot_sunburst(df, dim1, dim2):
+def relative_percentage_difference(a, b):
+    """Calculate the percentage difference relative to the first value."""
+    if a == 0:  # Avoid division by zero
+        raise ValueError("The first value cannot be zero.")
+    return int((a - b) / (a) * 100)
 
-	fig = px.sunburst(df, path = [dim1, dim2])
-
-	fig.update_layout(
-		title="Cluster and Income Level Sunburst",
-		margin=dict(t=20, l=0, r=0, b=0)
-	)
-
-	return fig
-
-
-def plot_bubble(df, dimension, metric):
+@st.cache_data
+def plot_bar(df, dimension, colors):
 
 	dim_title = dimension.replace('_', ' ').title()
 
-	grouped = df.groupby(['cluster', dimension], observed=True)[[metric, 'id']].agg({metric: 'mean', 'id': 'count'}).round(0).reset_index()
-	size = grouped['id'].tolist()
+	data = df.groupby(['cluster', dimension]).size().reset_index(name='count')
+	clusters = data['cluster'].unique().tolist()
 
-	metric_mean = grouped[metric].mean()
-
-	fig = go.Figure(data=[go.Scatter()])
-
-	for cluster, cluster_data in grouped.groupby('cluster'):
-		fig.add_trace(go.Scatter(
-			x=cluster_data[dimension].tolist(),
-			y=cluster_data[metric].tolist(),
-			mode='markers',
-			marker=dict(
-				size=size,
-				sizemode='area',
-				sizeref=2.*max(size)/(60.**2),
-				sizemin=4,
-				#color=grouped[cluster]#.unique()
-			),
-			name=f"Cluster {cluster}"
-			)
-		)
+	fig = make_subplots(
+		rows=len(clusters),
+		cols=1,
+		shared_xaxes=True,
+		vertical_spacing=0.03
+	)
 
 	fig.update_layout(
 		title=dict(
-			text=f"Spending Score and {dim_title}",
+			text=f"Count per {dim_title}",
 			x=0,
 			y=1,
 			xanchor='left',
@@ -61,88 +42,18 @@ def plot_bubble(df, dimension, metric):
 		),
 		showlegend=True,
 		legend=dict(
-			orientation='h',
-			x=0,
-			y=-.2
+			x=1,
+			y=1
+		),
+		margin=dict(
+			l=0,
+			r=0,
+			t=50,
+			b=0
 		)
-	)
-
-	fig.add_annotation(
-		xref='paper',
-		yref='paper',
-		xanchor='left',
-		yanchor='top',
-		x=-0.03,
-		y=1.2,
-		text="* The bubble size indicates the size of each group.",
-		showarrow=False,
-		align='left'
-	)
-
-	fig.update_yaxes(
-		range=[30, 70]
-	)
-
-	fig.add_hline(
-		y=metric_mean,
-		line_dash='dot',
-		line_color='white',
-		label=dict(
-			text='Mean',
-			textposition='start',
-			font=dict(
-				color='white'
-			)
-		)
-	)
-
-	return fig
-
-
-def relative_percentage_difference(a, b):
-    """Calculate the percentage difference relative to the first value."""
-    if a == 0:  # Avoid division by zero
-        raise ValueError("The first value cannot be zero.")
-    return int((a - b) / (a) * 100)
-
-
-def plot_bar(df, dimension):
-
-	dim_title = dimension.replace('_', ' ').title()
-
-	data = df.groupby(['cluster', dimension]).size().reset_index(name='count')
-	clusters = data['cluster'].unique()
-
-	fig = make_subplots(
-		rows=len(clusters),
-		cols=1,
-		shared_xaxes=True,
-		vertical_spacing=0.1
-	)
-
-	fig.update_layout(
-	title=dict(
-		text=f"Count per {dim_title}",
-		x=0,
-		y=1,
-		xanchor='left',
-		yanchor='top'
-	),
-	showlegend=False,
-	legend=dict(
-		orientation='h',
-		x=0,
-		y=-.2
-	),
-	margin=dict(
-		l=120,
-		r=0,
-		t=60,
-		b=0
-	)
 )
 
-	for i, cluster in enumerate(clusters):
+	for i, (cluster, color) in enumerate(zip(clusters, colors)):
 
 		cat_df = data[data['cluster'] == cluster].sort_values(by='count')
 
@@ -150,9 +61,11 @@ def plot_bar(df, dimension):
 			x=cat_df['count'],
 			y=cat_df[dimension],
 			orientation='h',
-			name=f"Cluster {cluster}",
+			name=f"Segment {cluster}",
 			text=cat_df['count'],
-			textfont=dict(color='white')
+			textfont=dict(color='white'),
+			textposition='outside',
+			marker=dict(color=f"{color}")
 		),
 		row=i+1,
 		col=1)
@@ -168,80 +81,78 @@ def plot_bar(df, dimension):
 			col=1
 		)
 
-		fig.add_annotation(
-			xref='paper',
-			yref='y' + str(i + 1),
-			#xanchor='right',
-			x=-0.50,
-			y=cat_df[dimension].iloc[len(cat_df) // 2],
-			text=f"S.{cluster}",
-			showarrow=False,
-			font=dict(size=20),
-			align='left'
+		fig.update_xaxes(
+			showticklabels=False
 		)
 
 	return fig
 
+@st.cache_data
+def plot_bubble(df, dimension, metric, colors):
 
-def test(df, dimension, metric):
-
-	#dim_title = dimension.replace('_', ' ').title()
 	met_title = metric.replace('_', ' ').title()
 
 	data = df.groupby(['cluster', dimension], observed=True)[[metric, 'id']].agg({metric: 'mean', 'id': 'count'}).round(0).reset_index()
 	clusters = data['cluster'].unique()
-	size = data['id'].tolist()
-	metric_mean = data[metric].mean()
 
 	fig = make_subplots(
 		rows=len(clusters),
 		cols=1,
 		shared_xaxes=True,
-		vertical_spacing=0.1
+		vertical_spacing=0.03
 	)
 
 	fig.update_layout(
-	title=dict(
-		text=met_title,
-		x=0,
-		y=1,
-		xanchor='left',
-		yanchor='top'
-	),
-	showlegend=False,
-	#legend=dict(
-	#	orientation='h',
-	#	x=0,
-	#	y=-.2
-	#),
-	margin=dict(
-		l=0,
-		r=0,
-		t=60,
-		b=0
-	)
+		title=dict(
+			text=met_title,
+			x=0,
+			y=1,
+			xanchor='left',
+			yanchor='top'
+		),
+		showlegend=True,
+		legend=dict(
+			#orientation='h',
+			x=1,
+			y=1
+		),
+		margin=dict(
+			l=0,
+			r=0,
+			t=50,
+			b=0
+		)
 )
 
-	for i, cluster in enumerate(clusters):
+	for i, (cluster, color) in enumerate(zip(clusters, colors)):
 
 		cat_df = data[data['cluster'] == cluster].sort_values(by=metric)
 
 		fig.add_trace(go.Scatter(
 			x=cat_df[metric].tolist(),
 			y=cat_df[dimension].tolist(),
-			mode='markers',
-			#marker=dict(
-				#size=size,
-				#sizemode='area',
-				#sizeref=2.*max(size)/(15.**2),
-				#sizemin=50,
-				#color=grouped[cluster]#.unique()
-			#),
-			name=f"Cluster {cluster}"
+			mode='markers+text',
+			marker=dict(color=color),
+			text=cat_df[metric], ######
+			textfont=dict(color='white'),
+			textposition='middle right',
+			name=f"Segment {cluster}"
 			),
-
 			row=i+1,
 			col=1)
+
+		# Add line traces for each point
+		for j in range(len(cat_df)):
+			fig.add_trace(go.Scatter(
+				x=[0, cat_df[metric].iloc[j]],  # Line from 0 to the x value
+				y=[cat_df[dimension].iloc[j], cat_df[dimension].iloc[j]],  # Same y-value
+				mode='lines',
+				line=dict(color=color, width=2),  # Adjust line properties
+				showlegend=False  # Avoid duplicate legend entries
+			),
+				row=i+1,
+				col=1
+			)		
 		
 		fig.update_yaxes(
 			showline=True,
@@ -251,9 +162,15 @@ def test(df, dimension, metric):
 			ticklen=7,
 			tickcolor='white',
 			row=i+1,
-			col=1
+			col=1,
+			showgrid=False
 		)
 
-		fig.update_traces(marker_size=10)
+		fig.update_xaxes(
+			showticklabels=False
+		)
+
+		fig.update_traces(marker_size=15)
 
 	return fig
+
